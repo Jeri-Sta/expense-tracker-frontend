5525240d8c156cf6c3289a1d1345dedaf3d34669
Jeriel <jerielstarosky@hotmail.com>
2025-02-07 12:07:26 -0300

    page-frame

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MenuComponent } from './menu/menu.component';
 import { RouterModule } from '@angular/router';
+import { PageFrameComponent } from './page-frame/page-frame.component';
 
 @NgModule({
-  declarations: [MenuComponent],
+  declarations: [MenuComponent, PageFrameComponent],
   imports: [CommonModule, RouterModule],
   providers: [],
-  exports: [MenuComponent],
+  exports: [MenuComponent, PageFrameComponent],
 })
 export class ComponentsModule {}
---
+++ src/app/components/page-frame/page-frame.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ComponentsModule } from '../components.module';
+
+@Component({
+  selector: 'ep-page-frame',
+  templateUrl: './page-frame.component.html',
+  styleUrl: './page-frame.component.css',
+  standalone: false,
+})
+export class PageFrameComponent {}
