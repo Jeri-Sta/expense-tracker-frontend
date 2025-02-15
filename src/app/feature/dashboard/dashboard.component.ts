9fe458fcf4fba2d6c306ad6b4cc75143cbc5b61a
Jeriel <jerielstarosky@hotmail.com>
2025-02-14 22:38:31 -0300

    #18 - seletor de datas

--- src/app/feature/dashboard/dashboard.component.ts
+++ src/app/feature/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import moment from 'moment';
+import 'moment/locale/pt-br';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,4 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.css',
   standalone: false,
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  currentDate: Date = new Date();
+
+  formatDate(date: Date): string {
+    return moment(date).locale('pt-br').format('MMM/YYYY');
+  }
+
+  nextMonth(): void {
+    this.currentDate.setMonth(this.currentDate.getMonth() + 1);
+  }
+
+  previousMonth(): void {
+    this.currentDate.setMonth(this.currentDate.getMonth() - 1);
+  }
+}
